a54a119971cef69bc35d22b4aa2fee71ce14a220
voiceinthedark <darkrisingforce@gmail.com>
2023-07-28 16:12:21 +0300

    Added dependencies, functioning inertia+vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
-import './bootstrap';
+import { createApp, h } from "vue";
+import { createInertiaApp, Link, Head } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import Layout from './Layouts/Layout.vue';
+
+createInertiaApp({
+    resolve: (name) =>
+    {// set default layout
+        const page = resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob("./Pages/**/*.vue")
+        );
+        page.then((component) => {
+            component.default.layout = component.default.layout || Layout;
+        });
+        return page;
+    },
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .component('Link', Link)
+            .component('Head', Head)
+            .mount(el);
+    },
+    title: (title) => (title ? `${title} - Markdown Blog` : "Markdown Blog"),
+});
